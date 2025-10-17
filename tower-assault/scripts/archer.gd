9a731d2cf1d5cc9f8f93360787802073751d4ee3
Ian Crowe <icrowe1794@gmail.com>
2025-10-16 23:04:23 -0500

    Overlapping Buildings & Location
    Partial fix, will need to adjust later

--- tower-assault/scripts/archer.gd
+++ tower-assault/scripts/archer.gd
@@ -2,8 +2,11 @@ extends StaticBody2D
 
 @export var proj_scene: PackedScene
 
+var parent_node
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	parent_node = get_parent()
 	$AttackTimer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,3 +22,9 @@ func _on_attack_timer_timeout():
 	
 	# Spawn the projectile by adding it to the Main scene.
 	add_child(projectile)
+
+func _on_mouse_entered() -> void:
+	parent_node.can_build = false
+
+func _on_mouse_exited() -> void:
+	parent_node.can_build = true
--- tower-assault/scripts/main.gd
+++ tower-assault/scripts/main.gd
@@ -5,6 +5,8 @@ extends Node
 var score
 var lives
 var build_tower_display
+var mouse_pos
+var can_build = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,18 +15,13 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float):
 	if Input.is_action_just_pressed(&"left_click") and build_tower_display:
-		build_tower(get_viewport().get_mouse_position())
+		mouse_pos = get_viewport().get_mouse_position()
+		if can_build:
+			build_tower(mouse_pos)
+			can_build = false
 	if Input.is_action_just_pressed(&"escape") and build_tower_display:
 		build_tower_display = false
 		$HUD.cancel_build_tower()
-	
-#func _unhandled_input(event):
-	#if event is InputEventKey:
-		#if Input.is_action_just_pressed(&"left_click") and build_tower_display:
-			#build_tower(get_viewport().get_mouse_position())
-		#if Input.is_action_pressed(&"escape") and build_tower_display:
-			#build_tower_display = false
-			#$HUD.cancel_build_tower()
 
 func new_game():
 	score = 0
@@ -85,3 +82,9 @@ func _on_score_timer_timeout():
 func _on_start_timer_timeout():
 	$MobTimer.start()
 	$ScoreTimer.start()
+
+func _on_build_zone_mouse_entered() -> void:
+	can_build = true
+
+func _on_build_zone_mouse_exited() -> void:
+	can_build = false
