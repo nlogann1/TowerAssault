039711a8ab99004c808b8505b54f708ee4bed478
Nathan Logan <nathanlogan246@gmail.com>
2025-10-25 13:37:52 -0500

    Soldier idle & attack animation fix. Deleted enemy path file

--- tower-assault/scripts/soldier.gd
+++ tower-assault/scripts/soldier.gd
@@ -6,6 +6,9 @@ var attack_damage = 10.0
 
 var target_monster = null
 
+enum {IDLE, WALK, ATTACK}
+var state = IDLE
+
 @onready var anim_sprite = $AnimatedSprite2D
 @onready var attack_timer = $AttackTimer
 @onready var health_bar = $HealthBar
@@ -16,58 +19,50 @@ func _ready():
 	health_bar.value = health
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	
-	# Only look for a new and closer target if our attack timer is stopped.
-	# but locks it onto a target once it starts attacking.
-	if attack_timer.is_stopped():
-		find_nearest_target() # This function updates target_monster
-	
-
-	if is_instance_valid(target_monster):
+	if not is_instance_valid(target_monster):
+		find_nearest_target()
+		state = IDLE
+	else:
 		var distance = global_position.distance_to(target_monster.global_position)
-		
-		# Attack range is 30 pixels.
-		if distance > 30:
-			# Seek Behavior
-			attack_timer.stop() 
+		if distance > 65:
+			state = WALK
+		else:
+			# Target is in range.
+			if state != ATTACK:
+				# If we aren't already attacking, start our first attack NOW.
+				state = ATTACK
+				do_attack()
+	
+	match state:
+		IDLE:
+			velocity = Vector2.ZERO
+			anim_sprite.play("idle")
+			
+		WALK:
 			velocity = global_position.direction_to(target_monster.global_position) * speed
 			anim_sprite.play("walk")
 			anim_sprite.flip_h = (velocity.x < 0)
-		else:
-			# Attack Behavior 
-			velocity = Vector2.ZERO # Stop moving
 			
-			# Start the attack timer if it's not already running
-			if attack_timer.is_stopped():
-				anim_sprite.play("idle")
-				attack_timer.start()
+		ATTACK:
+			velocity = Vector2.ZERO
 			
-	else:
-		velocity = Vector2.ZERO
-		anim_sprite.play("idle")
-		
 	move_and_slide()
 
 
 func find_nearest_target():
 	var nearest_mob = null
 	var min_distance = INF 
-	
-	# Get all enemies in our detection range
 	var nearby_bodies = detection_range.get_overlapping_bodies()
 	
 	for body in nearby_bodies:
 		if body.is_in_group("enemies"):
-			# Check its distance
 			var distance = global_position.distance_to(body.global_position)
-			
-			# If this one is closer than the last one, it's our new target
 			if distance < min_distance:
 				min_distance = distance
 				nearest_mob = body
 	
-	# This will either set the nearest mob, or set 'null' if none are in range
 	target_monster = nearest_mob
 
 
@@ -77,14 +72,22 @@ func take_damage(amount):
 	if health <= 0:
 		queue_free()
 
-func _on_attack_timer_timeout():
+func do_attack():
 	if is_instance_valid(target_monster):
+		state = ATTACK 
 		anim_sprite.play("attack")
 		target_monster.damage_taken(attack_damage)
-		attack_timer.start() # Restart timer for the next attack
+		attack_timer.start() # Start the timer after attacking
 	else:
-		attack_timer.stop()
+		# Target died before we could attack
+		state = IDLE
+
+
+func _on_attack_timer_timeout():
+	# Timer is done, so we attack again.
+	do_attack()
 
 func _on_animated_sprite_2d_animation_finished():
 	if anim_sprite.animation == "attack":
+		# Attack animation is done, just go to idle.
 		anim_sprite.play("idle")
