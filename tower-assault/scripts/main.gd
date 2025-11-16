c38d3a5dc53739332f22bc5cfae86130e9b7129c
Nathan Logan <nathanlogan246@gmail.com>
2025-11-15 21:55:15 -0600

    Update main.gd

--- tower-assault/scripts/main.gd
+++ tower-assault/scripts/main.gd
@@ -44,9 +44,9 @@ var current_cost = 150
 var base_upgrade_costs = [150, 400, 99999]
 
 var wave_data = [ 
-	{ "mob_type": "boss", "mob_count": 1, "mob_delay": 1.0 },
 	{ "mob_type": "normal", "mob_count": 8, "mob_delay": 1.5 },
 	{ "mob_type": "ghoul", "mob_count": 5, "mob_delay": 2.0 },
+	{ "mob_type": "boss", "mob_count": 1, "mob_delay": 1.0 },
 	{ "mob_type": "normal", "mob_count": 8, "mob_delay": 1.5 },
 	{ "mob_type": "ghoul", "mob_count": 3, "mob_delay": 3.0 },
 	{ "mob_type": "normal", "mob_count": 15, "mob_delay": 0.8 },
